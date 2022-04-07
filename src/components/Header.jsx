9b48c25f06e712ee85071663f2f5a6bd8d0d6833
Yoimel Rojas <yoimeldev@gmail.com>
2022-04-07 10:45:52 -0400

    add Header and fontFamily default

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header className='my-5 text-center text-2xl font-medium'>
+            <h1>IP Address Tracker</h1>
+        </header>
+    )
+}
