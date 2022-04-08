9b4498bd8004fe5e6fb13df5d37bc64785aa1c4a
Yoimel Rojas <yoimeldev@gmail.com>
2022-04-08 15:03:58 -0400

    add transition

--- src/components/AddressInfo.jsx
+++ src/components/AddressInfo.jsx
@@ -6,7 +6,7 @@ export const AddressInfo = ({ addressInfo }) => {
 
     return (
         <div
-            className='flex flex-col md:flex-row justify-center items-center md:items-start gap-5 w-80 md:w-[95%] xl:w-[45rem] mx-auto mt-6 py-6 bg-white rounded-2xl'>
+            className='flex flex-col md:flex-row justify-center items-center md:items-start gap-5 w-80 md:w-[95%] xl:w-[45rem] mx-auto mt-6 py-6 bg-white rounded-2xl transition-all'>
             <div className='text-center md:text-left md:pr-10 md:border-r-2'>
                 <p className="text-[#969696] text-xs font-bold font tracking-widest mb-1">
                     IP ADDRESS
