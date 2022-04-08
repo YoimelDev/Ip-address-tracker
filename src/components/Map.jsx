a2d88de180ab925391e9b22590b2b9a05348d6e2
Yoimel Rojas <yoimeldev@gmail.com>
2022-04-08 10:10:18 -0400

    add map component and styles

---
+++ src/components/Map.jsx
@@ -0,0 +1,30 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Icon } from 'leaflet'
+
+const iconLocation = new Icon({
+    iconUrl: "../src/img/icon-location.svg",
+    iconSize: [40, 60]
+});
+
+export const Map = () => {
+    const position = [51.505, -0.09]
+
+    return (
+        <MapContainer
+            style={{ zIndex: -1 }}
+            center={position}
+            zoom={13}
+            scrollWheelZoom={false}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+                position={position}
+                icon={iconLocation}
+            />
+        </MapContainer>
+
+    )
+}
