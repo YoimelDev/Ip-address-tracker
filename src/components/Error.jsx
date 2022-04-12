21115390c6253e3cae4c09113f60edafa70cf773
Yoimel Rojas <yoimeldev@gmail.com>
2022-04-12 09:20:36 -0400

    add responsive width

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Error = ({ errorMessage }) => {
     return (
-        <div className='bg-red-800 w-[30%] mx-auto py-2 flex justify-center text-white font-medium rounded-2xl'>
+        <div className='bg-red-800 w-[90%] sm:w-[50%] md:w-[30%]  mx-auto py-2 flex justify-center text-white font-medium rounded-2xl'>
             {errorMessage}
         </div>
     )
